7b565e20dc29dd937c944cb3c6efac65b1262af6
Faisal jamal <fjamal862@gmail.com>
2026-05-07 12:10:36 +0530

    Pushing my first app to git

--- homeWorks/may'4.js
+++ homeWorks/may'4.js
@@ -29,12 +29,12 @@
 
 // console.log(arr.flat());
 
-// question 3.
-let arr = [1,2,2,3,3,3, 4,4,4,4, 5,5,5,5,5];
-let arr1 = [];
-for (let i of arr) {
-    if (!arr1.includes(i)) {
-        arr1.push(i);
-    }
-}
-console.log(arr1);
+// // question 3.
+// let arr = [1,2,2,3,3,3, 4,4,4,4, 5,5,5,5,5];
+// let arr1 = [];
+// for (let i of arr) {
+//     if (!arr1.includes(i)) {
+//         arr1.push(i);
+//     }
+// }
+// console.log(arr1);
